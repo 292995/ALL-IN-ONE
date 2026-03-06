c12bf70c6e37fc68b61815654a5f1d45d117bc99
nandani <radhnandani.prasad29@gmail.com>
2026-03-06 10:03:58 +0000

    add string functions

--- InputOuput.py
+++ InputOuput.py
@@ -2,5 +2,21 @@
 
 age = int(input("Please enter the age of user :"))
 age_in_10_years = age + 10
-print("In 10 years your will be : " ,age_in_10_years)
-print("Hello, World!")
+print("In 10 years your will be : " , age_in_10_years)
+
+
+#program about input the name of user and output its name in upper case.
+
+name = input("Please enter the name of the user :")
+print("Your name in upper case is : ", name.upper())
+
+#program about input the name of user and output its name in lower case.
+name = input("Please enter the name of the user :")
+print("Your name in lower case is : ", name.lower())
+
+#program about input the name of user and output its name in title case.
+name = input("Please enter the name of the user :")
+print("Your name in title case is : ", name.title())
+
+#program about input the name of user and output its name in reverse order.
+name = input("Please enter the name of the user :")
